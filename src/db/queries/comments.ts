f4507fa62967d8f2bda40b782461fc9d35849881
iamluckycheema <luckycheema002337@gmail.com>
2024-08-31 10:39:36 +0500

    setup request memorization/cache memorization for comments

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,27 +1,27 @@
 import type { Comment } from '@prisma/client'
 import { db } from '@/db'
-import { log } from 'console'
+import { cache } from 'react'
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null }
 }
 
-export function fetchCommentsByPostId(
-  postId: string
-): Promise<CommentWithAuthor[]> {
-  console.log("makin' query")
+export const fetchCommentsByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    console.log("makin' query")
 
-  return db.comment.findMany({
-    where: {
-      postId,
-    },
-    include: {
-      user: {
-        select: {
-          name: true,
-          image: true,
+    return db.comment.findMany({
+      where: {
+        postId,
+      },
+      include: {
+        user: {
+          select: {
+            name: true,
+            image: true,
+          },
         },
       },
-    },
-  })
-}
+    })
+  }
+)
